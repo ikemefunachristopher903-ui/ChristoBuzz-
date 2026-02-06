e3e98993e385c71d5524c96468cbbc40b1408065
ikemefunachristopher903-ui <ikemefunachristopher903@gmail.com>
2026-02-06 10:05:12 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,10 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+// supabase.js
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+const SUPABASE_URL = https://aiwigwfsajdnwfpcziqf.supabase.co
+const SUPABASE_ANON_KEY = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpd2lnd2ZzYWpkbndmcGN6aXFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ5NzExMTksImV4cCI6MjA4MDU0NzExOX0.7Mk1IUVbxhCnwQ4HYo-02G4W2pNoBtSbRe8X4UizstA
 
 export const supabase = createClient(
-  "https://aiwigwfsajdnwfpcziqf.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpd2lnd2ZzYWpkbndmcGN6aXFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ5NzExMTksImV4cCI6MjA4MDU0NzExOX0.7Mk1IUVbxhCnwQ4HYo-02G4W2pNoBtSbRe8X4UizstA"
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
 );
